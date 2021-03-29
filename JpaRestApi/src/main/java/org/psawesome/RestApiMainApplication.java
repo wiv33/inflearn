d464d55eb2dc1f037781025272725edfbeb775df
PSK <wiv821253@gmail.com>
2021-03-29 12:07:07 +0900

    init jpa rest api

---
+++ JpaRestApi/src/main/java/org/psawesome/RestApiMainApplication.java
@@ -0,0 +1,13 @@
+package org.psawesome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiMainApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestApiMainApplication.class, args);
+    }
+
+}
---
+++ JpaRestApi/src/main/java/org/psawesome/page/HomeController.java
@@ -0,0 +1,18 @@
+package org.psawesome.page;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.reactive.result.view.Rendering;
+import reactor.core.publisher.Mono;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping("/hello")
+    public Mono<Rendering> homePage() {
+        return Mono.just(Rendering.view("hello")
+                .modelAttribute("data", "Hello World!!")
+                .build()
+        );
+    }
+}
